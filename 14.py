6be8a6bb516589514371be8c506436a13bddf6bd
papaklim <papaklim@gmail.com>
2019-10-26 23:28:34 +0300

    edit 14.py

---
+++ 14.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+
+
+browser = webdriver.Chrome()
+
+url = "http://suninjuly.github.io/explicit_wait2.html"
